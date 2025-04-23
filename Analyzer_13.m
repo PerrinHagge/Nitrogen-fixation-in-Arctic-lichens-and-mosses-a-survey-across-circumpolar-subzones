ae7c94e57c90af927bb12a6b99c305f49ae93c77
PerrinHagge <perrin.hagge@gmail.com>
2025-04-23 14:18:26 -0400

    Update Analyzer_13.m

--- Analyzer_13.m
+++ Analyzer_13.m
@@ -1,58 +1,83 @@
-% Provide the file path of the Excel file
-file_path = 'Lichens.xlsx';
+% Load data from Lichens.xlsx
+%data = readtable('Bryophytes.xlsx');
+%data = readtable('Bryophytes_FR.xlsx');
 
-% Read the Excel file
-data = readtable(file_path);
+data = readtable('Lichens.xlsx');
+%data = readtable('Lichens_FR.xlsx');
 
-% Extract data table
-b_C2H4 = data{:, 'bC2H4'}; 
-m_C2H4 = data{:, 'fC2H4'}; 
-Type = data{:, 'Genus'}; 
-Location = data{:, 'Location'};
-dry_w = data{:, 'Dry_Weight'};
-wet_w = data{:, 'Wet_Weight'};
-T_uc = data{:, 'Temperature'};
-s_name = data{:, 'Sample'};
-Lat = data{:, 'Latitude'};
-Long = data{:, 'Longitude'};
-Collection = data{:, 'Collection'};
-Subzone = data{:, 'Subzone'};
-Nb = data{:, 'Nb'};
-Nm = data{:, 'Nm'};
-pv_bC2H4 = data{:, 'pv_bC2H4'};
-pv_mC2H4 = data{:, 'pv_fC2H4'};
+% Extract required variables
+T_C = data.Temperature;      % in Celsius
+b = data.bC2H4;              % background C2H4
+m = data.fC2H4;              % fixation C2H4 (equivalent to slope)
+dry_w = data.Dry_Weight;     % dry weight in g
 
-% Subtract values in column A from column B to create variable C2H4
-b_C2H4(b_C2H4 < 0) = 0;
-C2H4 = m_C2H4 - b_C2H4;
+% Apply offsets to b and m
+b = b + 0.1253;
+m = m + 112.82;
 
-% Zero Correct Data
-C2H4_zc = C2H4;
-C2H4_zc(C2H4 < 0) = 0;
+% Constants
+P = 1;                       % atm
+Vc = 0.5674;                 % L
+R = 0.0821;                  % L·atm·mol⁻¹·K⁻¹
+Ratio = 3;                   % C2H4:N2 ratio
+T_K = T_C + 273.15 + 1.21707317;          % convert to Kelvin
 
-% Make rates per hour
-C2H4_24 = C2H4_zc ./ 24;
+% Absolute uncertainties
+rel_err_m = 0.03;            % 3%
+rel_err_b = 0.03;            % 3%
+delta_Vc = 0.01135;          % L
+delta_T = 0.5;               % K
+delta_dry_w = 0.001;         % g
+rel_err_P = 0.007;           % 0.7% uncertainty in pressure
 
-% Calculate Volume of C2H4 in the chamber
-C2H4_V = (C2H4_24 .* 0.5674)./ 1000000000;
+% Preallocate output
+N_dw = zeros(size(m));
+delta_N_dw = zeros(size(m));
 
-% Calculate Corrected Temp in Celsius
-T = T_uc + 1.21707317;
+% Loop through each row of data
+for i = 1:length(m)
+    m_i = m(i);
+    b_i = b(i);
+    dw_i = dry_w(i);
+    T_i = T_K(i);
 
-% Calculate Temp in Kelvin
-T_K = T + 273.15; 
+    % Set b to 0 if it's negative
+    if b_i < 0
+        b_i = 0;
+    end
 
-% Calculate mols of C2H4 in the chamber
-C2H4_m = (C2H4_V .* 1)./ (0.0821 .* T_K ); % 1 is the pressure in atmosphere
+    % Set N_dw and uncertainty to 0 if invalid m
+    if m_i < b_i || m_i < 0
+        N_dw(i) = 0;
+        delta_N_dw(i) = 0;
+    else
+        % Calculate N_dw
+        num = (m_i - b_i) * Vc * P;
+        denom = R * T_i * Ratio * dw_i;
+        N_dw(i) = (1/12) * num / denom;
 
-% Calculate nano mols of N2 in the chamber
-N2_nm = (C2H4_m .* 1000000000)./ 3;
+        % Relative uncertainty in (m - b)
+        delta_m = rel_err_m * m_i;
+        delta_b = rel_err_b * b_i;
+        rel_err_mb = sqrt(delta_m^2 + delta_b^2) / abs(m_i - b_i);
 
-% Calculate nano mols of N in the chamber
-N_nm = N2_nm.* 2;
+        % Relative errors for other terms
+        rel_err_Vc = delta_Vc / Vc;
+        rel_err_T = delta_T / T_i;
+        rel_err_dw = delta_dry_w / dw_i;
 
-% Standardize to Dry Weight
-N_dw = N_nm ./ dry_w;
+        % Total relative uncertainty in N_dw (includes P)
+        rel_err_N_dw = sqrt(rel_err_mb^2 + rel_err_Vc^2 + ...
+                            rel_err_T^2 + rel_err_dw^2 + rel_err_P^2);
 
-% Water Content
-wc = ((wet_w - dry_w) ./ dry_w) * 100;
+        % Absolute uncertainty
+        delta_N_dw(i) = N_dw(i) * rel_err_N_dw;
+    end
+end
+
+% Store results in the table
+data.N_dw = N_dw;
+data.delta_N_dw = delta_N_dw;
+
+% Optional: save to a new file
+writetable(data, 'Lichens_with_N_dw.xlsx');
